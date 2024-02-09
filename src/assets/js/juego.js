c3e93f2ffc18884165fce20fa33c097a65dab7f4
Carlos Concepcion <concep131@gmail.com>
2024-02-09 17:30:17 +0000

    Draw card done

--- src/assets/js/juego.js
+++ src/assets/js/juego.js
@@ -1,9 +1,11 @@
+import { shuffle } from "underscore"
+
 let deck = []
 const types = ['C', 'D', 'H', 'S']
 const specials = ['A', 'J', 'Q', 'K']
 
 const createDeck = () => {
-    for (i = 2; i <= 10; i++) {
+    for (let i = 2; i <= 10; i++) {
         for (let type of types) {
             deck.push(i + type)
         }
@@ -14,6 +16,35 @@ const createDeck = () => {
         }
     }
     console.log('deck', deck)
+    deck = shuffle(deck)
+    console.log(deck);
+    return deck
+}
+
+// Esta funcion me permite pedir una carta
+
+const drawCard = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck'
+    }
+
+    let card = deck.pop()
+    return card
+}
+
+const cardValue = (card) => {
+    const value = card.match(/[2-9]|10|A|J|Q|K/)
+    //otra manera card.substring(0, card.length -1)
+    return (isNaN(value)) ?
+        (value === 'A') ? 11 : 10
+        :
+        value * 1
 }
 
 createDeck()
+
+
+const value = cardValue(drawCard())
+console.log({ value })
+
